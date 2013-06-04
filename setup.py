aa7baf8efd779db831e6966ee69834dcf64cd482
Adam Donahue <adam@appnexus.com>
2013-06-04 11:31:56 -0400

    Initial.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='nodes',
+      version='alpha',
+      description='An easy-to-use graph model for Python.',
+      classifiers=[
+          "Programming Language :: Python",
+          ("Topic :: Software Development :: Libraries :: Python Modules")
+          ],
+      keywords='pynodes nodes graph functional',
+      author='Adam M. Donahue',
+      author_email='adam.donahue@gmail.com',
+      packages=['nodes']
+      )
